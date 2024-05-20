43ca12b448a2c7138f620fad7673c4f738067a6f
LupineReX <student.148242@worcesterschols.net>
2024-05-20 13:43:10 -0400

    gun works

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("HP")]
+    public float health = 50f;
+    
+    public void TakeDamage (float amount)
     {
-        
+        health -= amount;
+        if (health <0f)
+        {
+            Die();
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    void Die()
     {
-        
+        Destroy(gameObject);
     }
 }
