e4100d85eb86ea69aefbdd0cc16e4f0ffeed2ae1
LupineReX <student.148242@worcesterschols.net>
2024-05-21 11:31:30 -0400

    d

---
+++ Assets/Scripts/Recoil.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Recoil : MonoBehaviour
+{
+    [Header("Rotations")]
+    private Vector3 currentRotation;
+    private Vector3 targetRotation;
+    [Header("Hipfire Recoil")]
+    [SerializeField] private float recoilX;
+    [SerializeField] private float recoilY;
+    [SerializeField] private float recoilZ;
+    [Header("Recoil Settings")]
+    [SerializeField] private float snappiness;
+    [SerializeField] private float snappiness;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
